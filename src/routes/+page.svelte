7e52a8336c598dba1ccee339d0c2e6a5a84d2048
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2024-08-16 23:28:47 +0530

    added loading icon when prod-filters are applied

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,13 +11,21 @@
 
     export let data: PageData;
     export let form: ActionData;
+
+    let loading = false;
 </script>
 
 <div class="container my-2 mx-auto flex gap-2">
     <div id="sidebar" class="flex-shrink-0 p-3 border w-max">
         <h1 class="text-xl mb-2 font-semibold">Filters</h1>
         <hr>
-        <form bind:this={formElement} method="POST" action="?/filter" use:enhance={() => async ({ update }) => update({ reset: false })}>
+        <form bind:this={formElement} method="POST" action="?/filter" use:enhance={() => {
+            loading = true;
+            return async ({ update }) => {
+                await update({ reset: false });
+                loading = false;
+            }
+        }}>
             <fieldset class="my-2 text-sm">
                 <legend class="text-base font-semibold mb-2">Categories:</legend>
                 {#each data.categories as category}
@@ -37,18 +45,24 @@
                 </div>
             </fieldset>
             <hr>
-            <button type="submit" class="mt-3 w-full p-1 bg-green-700 text-white rounded-lg font-bold">Apply</button>
+            <input type="submit" class:cursor-not-allowed={loading} class:opacity-50={loading} disabled={loading} class="mt-3 w-full p-1 bg-green-700 text-white rounded-lg font-bold" value="Apply" />
         </form>
     </div>
-    <main class="w-full flex-grow border p-4">
-        <h1 class="font-semibold mb-2">Showing {#if form?.success}{form.products.length}{:else}{data.total}{/if} of {data.total} products</h1>
+    <main class="w-full flex-grow border p-4 relative">
+        <h1 class="font-semibold mb-2">Showing {#if form?.success || data.products}{form?.products.length ?? data.products.length}{:else}{data.total}{/if}<!-- of {data.total}--> products</h1>
         <hr>
-        <div id="search-results" class="grid justify-items-stretch grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-2 gap-2">
-            {#if form?.success}
-                {#each form?.products as product}
+        <div id="search-results" class:opacity-50={loading} class="grid justify-items-stretch grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-2 gap-2">
+            {#if form?.success || data.products}
+                {#each form?.products ?? data.products as product}
                     <ProductCard {...product}/>
                 {/each}
             {/if}
+        </div>
+        <div class:hidden={!loading} class:block={loading} class="absolute top-0 left-0 w-full h-full">
+            <svg class="animate-spin h-8 w-8 text-red-600 sticky top-1/2 left-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
         </div>
     </main>
 </div>
