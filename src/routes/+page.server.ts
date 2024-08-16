349567376507736e16efa0323f74c543c34e94a2
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2024-08-16 17:25:01 +0530

    called the business logic fn instead of hardcoding

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,20 +1,22 @@
-import type { PageServerLoad } from './$types';
-import type { Actions } from './$types';
+import { getProductByFilter, getTotalProductCount, getAllCategories } from '$lib/server/db_logic';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load: PageServerLoad = async () => {
-	return {
-        total: 20,
-        categories: [
-            "electronics",
-            "jewelery",
-            "men's clothing",
-            "women's clothing"
-        ]
+    return {
+        total: await getTotalProductCount(),
+        categories: await getAllCategories()
     }
 };
 
 export const actions = {
-	filter: async (event) => {
-		console.log(event)
+	filter: async ({ request }) => {
+		const req_filters = await request.formData();
+        const categories = req_filters.getAll('category') as Category[];
+        const price_cap = +`${req_filters.get('price-cap')}` as number;
+
+        return {
+            success: true,
+            products: await getProductByFilter(categories, price_cap)
+        }
 	},
 } satisfies Actions;
