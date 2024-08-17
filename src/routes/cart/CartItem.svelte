252fb164202fa76cac32e28dbdc933b1f33f3e14
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2024-08-17 22:26:45 +0530

    made the CartItems responsive for mobile devices

--- src/routes/cart/CartItem.svelte
+++ src/routes/cart/CartItem.svelte
@@ -8,24 +8,24 @@
     let prod: Product;
     
     let itemRequest = getProduct(pid);
-    itemRequest.then(p => prod = p)
+    itemRequest.then(p => prod = p);
     
     $: sum = quantity * (prod?.price ?? 0);
 </script>
 
 {#await itemRequest}
-<div class="flex w-full h-[170px] items-center justify-center">
+<div class="flex w-full h-[170px] items-center justify-center border-b">
     <svg class="animate-spin h-8 w-8 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
 </div>
 {:then product}
-<div class="flex w-full hover:bg-[#f6f6f6]">
+<div class="flex w-full hover:bg-[#f6f6f6] border-b">
     <div class="flex gap-2 p-3">
-        <div class="flex flex-col gap-5 h-full w-40 justify-between">
+        <div class="flex flex-col gap-2 sm:gap-5 h-full w-28 sm:w-40 justify-between">
             <div class="flex items-center justify-center grow-0 shrink-1">
-                <img class="max-w-full h-[100px]" src={product.image} alt="product image">
+                <img class="max-w-full h-[85px] sm:h-[100px]" src={product.image} alt="product image">
             </div>
             <div class="shrink-0 flex justify-center">
                 <button class:cursor-not-allowed={quantity < 2} class:opacity-50={quantity < 2} class="flex items-center justify-center aspect-square text-base bg-slate-100 hover:bg-slate-300 border border-[darkgray] text-center rounded-full w-[3ch]" disabled={quantity < 2} on:click={() => quantity--}><span>&ndash;</span></button>
@@ -36,9 +36,9 @@
     </div>
     <div class="flex flex-col p-2 justify-between w-full gap-2">
         <div>
-            <h1 class="mb-2 font-semibold text-xl">{product.title}</h1>
-            <div class="font-semibold mb-3">Price: &#8377;&nbsp;{product.price.toFixed(2)}</div>
-            <div class="text-sm flex gap-3">
+            <h1 class="sm:mb-2 font-semibold mb-0 text-lg sm:text-xl">{product.title}</h1>
+            <div class="font-semibold mb-1 sm:mb-3">Price: &#8377;&nbsp;{product.price.toFixed(2)}</div>
+            <div class="text-sm flex flex-col sm:flex-row gap-0 sm:gap-3">
                 <div>Rating: <strong>{product.rating.rate}</strong></div>
                 <div class="flex items-center gap-2">
                     <div class="flex">
