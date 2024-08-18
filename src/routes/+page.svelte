ad52be2b94fbb685443ddd47b99b5d1b703ea653
Azhar Ahmed <49714602+ahmedazhar05@users.noreply.github.com>
2024-08-18 14:47:31 +0530

    made it more mobile friendly & visually appealing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,62 +12,66 @@
     let loading = false;
 </script>
 
-<div class="container p-2 mx-auto flex flex-col sm:flex-row gap-2 items-start">
-    <details open id="sidebar" class="group flex-shrink-0 p-3 border sticky top-0 bg-white rounded z-10 w-full sm:w-max md:w-64">
-        <summary class="text-xl mb-0 group-open:mb-2 font-semibold flex justify-between items-center sm:group-open:pointer-events-none">
-            <span>Filters</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group-[&:not([open])]:hidden sm:hidden size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>                  
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group-open:hidden sm:group-open:block size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-            </svg>              
-        </summary>
-        <hr>
-        <form method="POST" action="?/filter" use:enhance={() => {
-            loading = true;
-            return async ({ update }) => {
-                await update({ reset: false });
-                loading = false;
-            }
-        }}>
-            <fieldset class="my-2 text-sm">
-                <legend class="text-base font-semibold mb-2">Categories:</legend>
-                {#each data.categories as category}
-                    {@const cid = category.replace(/[\W\s]+/, '_')}
+<div class="container px-2 mx-auto flex flex-col sm:flex-row sm:gap-2 items-start">
+    <div class="py-2 flex-shrink-0 z-10 w-full sm:w-max md:w-64 sticky top-0 bg-[gainsboro]">
+        <details open id="sidebar" class="group p-3 border bg-white rounded">
+            <summary class="text-xl mb-0 group-open:mb-2 font-semibold flex justify-between items-center sm:group-open:pointer-events-none">
+                <span>Filters</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group-[&:not([open])]:hidden sm:hidden size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>                  
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="group-open:hidden sm:group-open:block size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+                </svg>              
+            </summary>
+            <hr>
+            <form method="POST" action="?/filter" use:enhance={() => {
+                loading = true;
+                return async ({ update }) => {
+                    await update({ reset: false });
+                    loading = false;
+                }
+            }}>
+                <fieldset class="my-2 text-sm">
+                    <legend class="text-base font-semibold mb-2">Categories:</legend>
+                    {#each data.categories as category}
+                        {@const cid = category.replace(/[\W\s]+/, '_')}
+                        <div class="flex items-center gap-2">
+                            <input class="peer accent-[#ff2422] rounded" type="checkbox" name="category" id="category-{cid}" value={category} checked>
+                            <label for="category-{cid}" class="peer-checked:font-semibold">{toCamelCase(category)}</label>
+                        </div>
+                    {/each}
+                </fieldset>
+                <hr>
+                <fieldset class="my-2 text-sm">
+                    <legend class="text-base font-semibold mb-2">Price Limit:</legend>
                     <div class="flex items-center gap-2">
-                        <input class="peer accent-[#ff2422] rounded" type="checkbox" name="category" id="category-{cid}" value={category} checked>
-                        <label for="category-{cid}" class="peer-checked:font-semibold">{toCamelCase(category)}</label>
+                        <input type="range" name="price-cap" id="price-cap" class="w-full" step="100" min="0" max="1000" bind:value={cap}>
+                        <span>{cap}</span>
                     </div>
-                {/each}
-            </fieldset>
+                </fieldset>
+                <hr>
+                <input type="submit" class:cursor-not-allowed={loading} class:opacity-50={loading} disabled={loading} class="mt-3 w-full p-1 bg-green-700 text-white rounded font-bold" value="Apply" />
+            </form>
+        </details>
+    </div>
+    <div class="w-full flex-grow sm:pt-2 pb-2">
+        <main class="border p-4 relative bg-white rounded">
+            <h1 class="font-semibold mb-2">Showing {#if form?.success || data.products}{form?.products.length ?? data.products.length}{:else}{data.total}{/if}<!-- of {data.total}--> products</h1>
             <hr>
-            <fieldset class="my-2 text-sm">
-                <legend class="text-base font-semibold mb-2">Price Limit:</legend>
-                <div class="flex items-center gap-2">
-                    <input type="range" name="price-cap" id="price-cap" class="w-full" step="100" min="0" max="1000" bind:value={cap}>
-                    <span>{cap}</span>
-                </div>
-            </fieldset>
-            <hr>
-            <input type="submit" class:cursor-not-allowed={loading} class:opacity-50={loading} disabled={loading} class="mt-3 w-full p-1 bg-green-700 text-white rounded font-bold" value="Apply" />
-        </form>
-    </details>
-    <main class="w-full flex-grow border p-4 relative bg-white rounded">
-        <h1 class="font-semibold mb-2">Showing {#if form?.success || data.products}{form?.products.length ?? data.products.length}{:else}{data.total}{/if}<!-- of {data.total}--> products</h1>
-        <hr>
-        <div id="search-results" class:opacity-50={loading} class="grid justify-items-stretch grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-2 gap-2">
-            {#if form?.success || data.products}
-                {#each form?.products ?? data.products as product}
-                    <ProductCard {...product}/>
-                {/each}
-            {/if}
-        </div>
-        <div class:hidden={!loading} class:block={loading} class="absolute top-0 left-0 w-full h-full">
-            <svg class="animate-spin h-8 w-8 text-red-600 sticky top-1/2 left-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-            </svg>
-        </div>
-    </main>
+            <div id="search-results" class:opacity-50={loading} class="grid justify-items-stretch grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-2 gap-2">
+                {#if form?.success || data.products}
+                    {#each form?.products ?? data.products as product}
+                        <ProductCard {...product}/>
+                    {/each}
+                {/if}
+            </div>
+            <div class:hidden={!loading} class:block={loading} class="absolute top-0 left-0 w-full h-full">
+                <svg class="animate-spin h-8 w-8 text-red-600 sticky top-1/2 left-1/2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+            </div>
+        </main>
+    </div>
 </div>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -36,43 +36,47 @@
     </div>
 </div>
 {:else}
-<div class="container mx-auto my-0 flex flex-col lg:flex-row gap-2 p-2 items-start justify-center">
-    <div id="cart" class="flex flex-col border rounded-md w-full max-w-4xl bg-white">
-        <div class="font-semibold p-2 border-b">
-            <h1 class="text-xl inline-block">My Cart</h1>
-            <span class="text-lg">({$cart.size} product{$cart.size > 1 ? 's' : ''})</span>
-        </div>
-        <div>
-            {#each $cart as pid (pid)}
-            <CartItem {pid} bind:sum={itemSum[pid]}/>
-            {/each}
-        </div>
-        <div class="flex justify-end p-2">
-            <a href="/" class="bg-[lightblue] py-2 px-4 rounded-md w-full sm:w-auto text-center">Continue with shopping</a>
+<div class="container mx-auto my-0 flex flex-col lg:flex-row lg:gap-2 px-2 items-start justify-center">
+    <div class="w-full max-w-4xl py-2 bg-[gainsboro]">
+        <div id="cart" class="flex flex-col border rounded-md bg-white">
+            <div class="font-semibold p-2 border-b">
+                <h1 class="text-xl inline-block">My Cart</h1>
+                <span class="text-lg">({$cart.size} product{$cart.size > 1 ? 's' : ''})</span>
+            </div>
+            <div>
+                {#each $cart as pid (pid)}
+                <CartItem {pid} bind:sum={itemSum[pid]}/>
+                {/each}
+            </div>
+            <div class="flex justify-end p-2">
+                <a href="/" class="bg-[lightblue] py-2 px-4 rounded-md w-full sm:w-auto text-center">Continue with shopping</a>
+            </div>
         </div>
     </div>
-    <div id="order-summary" class="sticky top-0 flex flex-col max-w-full lg:max-w-[22rem] w-full rounded-md border bg-white">
-        <h1 class="text-lg p-2 border-b font-semibold">Price Details</h1>
-        <div class="grid gap-2 p-2 grid-cols-[auto_max-content]">
-            <span>Subtotal ({$cart.size} item{$cart.size > 1 ? 's' : ''})</span>
-            <span class="text-end">&#8377;&nbsp;{total}</span>
-
-            <span>Delivery charges</span>
-            {#if deliveryCharge}
-            <span>&#8377;&nbsp;{deliveryCharge}</span>
-            {:else}
-            <span class="text-green-700 font-semibold text-end">FREE</span>
-            {/if}
-            
-            <span>Discount <input class="font-mono px-2 py-0.5 rounded" type="text" size="10" placeholder="COUPON" bind:value={couponUsed}> {discountCoupons[couponUsed.toUpperCase()] ?? 0}% off</span>
-            <span class="text-end text-green-700">-&#8377;&nbsp;{discount.toFixed(2)}</span>
-        </div>
-        <div class="grid gap-2 p-2 grid-cols-[auto_max-content] border-t font-semibold">
-            <span>Total</span>
-            <span class="text-end">&#8377;&nbsp;{grandTotal.toFixed(2)}</span>
-        </div>
-        <div class="flex justify-end lg:justify-center border-t font-semibold p-2">
-            <button class="bg-[goldenrod] py-2 px-4 rounded-md w-full sm:w-auto lg:w-full text-white">Proceed to Checkout</button>
+    <div class="sticky top-0 max-w-full lg:max-w-[22rem] w-full lg:pt-2 pb-2">
+        <div id="order-summary" class="flex flex-col rounded-md border bg-white">
+            <h1 class="text-lg p-2 border-b font-semibold">Price Details</h1>
+            <div class="grid gap-2 p-2 grid-cols-[auto_max-content]">
+                <span>Subtotal ({$cart.size} item{$cart.size > 1 ? 's' : ''})</span>
+                <span class="text-end">&#8377;&nbsp;{total}</span>
+    
+                <span>Delivery charges</span>
+                {#if deliveryCharge}
+                <span>&#8377;&nbsp;{deliveryCharge}</span>
+                {:else}
+                <span class="text-green-700 font-semibold text-end">FREE</span>
+                {/if}
+                
+                <span>Discount <input class="font-mono px-2 py-0.5 rounded" type="text" size="10" placeholder="COUPON" bind:value={couponUsed}> {discountCoupons[couponUsed.toUpperCase()] ?? 0}% off</span>
+                <span class="text-end text-green-700">-&#8377;&nbsp;{discount.toFixed(2)}</span>
+            </div>
+            <div class="grid gap-2 p-2 grid-cols-[auto_max-content] border-t font-semibold">
+                <span>Total</span>
+                <span class="text-end">&#8377;&nbsp;{grandTotal.toFixed(2)}</span>
+            </div>
+            <div class="flex justify-end lg:justify-center border-t font-semibold p-2">
+                <button class="bg-[goldenrod] py-2 px-4 rounded-md w-full sm:w-auto lg:w-full text-white">Proceed to Checkout</button>
+            </div>
         </div>
     </div>
 </div>
